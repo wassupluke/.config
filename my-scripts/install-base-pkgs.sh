91fd980332dcafa0fd1515a3eed4634a8f20fada
Luke <wassupluke@gmail.com>
2024-10-21 12:37:06 -0600

    removes font zip after install

--- my-scripts/install-base-pkgs.sh
+++ my-scripts/install-base-pkgs.sh
@@ -4,10 +4,11 @@
 	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
 	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 	&& sudo apt update \
-	&& sudo apt install gh curl sway swaybg swayidle swaylock xdg-desktop-portal-wlr xwayland waybar brightnessctl feh rofi dunst grimshot neovim network-manager-gnome alacritty playerctl fonts-font-awesome neofetch btop copyq python3 pipx wlsunset -y \
+	&& sudo apt install gh curl sway swaybg swayidle swaylock xdg-desktop-portal-wlr xwayland waybar brightnessctl feh rofi mako-notifier grimshot neovim network-manager-gnome alacritty playerctl fonts-font-awesome neofetch btop copyq python3 pipx wlsunset -y \
 	&& pipx install autotiling \
 	&& pipx ensurepath \
 	&& eval "$(register-python-argcomplete pipx)" \
 	&& wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/IosevkaTerm.zip \
 	&& unzip "*.zip" "*.ttf" "*.otf" -d ${HOME}/.fonts \
-	; sudo fc-cache -f -v
+	; rm IosevkaTerm.zip \
+	&& sudo fc-cache -f -v
