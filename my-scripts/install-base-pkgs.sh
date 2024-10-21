f3e79055382fcc638e2bb4a63236b46ce26ff66e
Luke <wassupluke@gmail.com>
2024-10-21 09:18:01 -0600

    Update install-base-pkgs.sh

--- my-scripts/install-base-pkgs.sh
+++ my-scripts/install-base-pkgs.sh
@@ -4,4 +4,4 @@
 	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
 	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 	&& sudo apt update \
-	&& sudo apt install gh sway swaybg swayidle swaylock xdg-desktop-portal-wlr xwayland -y
+	&& sudo apt install gh sway swaybg swayidle swaylock xdg-desktop-portal-wlr xwayland waybar rofi neovim -y
