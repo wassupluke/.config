cb084e68433e6d1ac5d3a04725448453a9d5e9e3
Luke <wassupluke@gmail.com>
2024-10-22 13:59:00 -0600

    quit installing brave

--- my-scripts/install-base-pkgs.sh
+++ my-scripts/install-base-pkgs.sh
@@ -14,12 +14,6 @@
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg \
 \
 \
-&& sudo apt install software-properties-common apt-transport-https wget ca-certificates gnupg2 -y \
-&& wget -O- https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg | sudo gpg --dearmor | sudo tee /usr/share/keyrings/brave-browser-archive-keyring.gpg \
-&& echo deb [arch=amd64 signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main | sudo tee /etc/apt/sources.list.d/brave-browser-release.list \
-&& sudo apt update && sudo apt install brave-browser -y \
-\
-\
 && sudo ntpdate pool.ntp.org \
 && sudo hwclock --systohc --utc \
 \
