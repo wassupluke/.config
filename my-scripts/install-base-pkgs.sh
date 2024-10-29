70a801273c8ddfacbd004b752bed48ca3f56f316
Luke <wassupluke@gmail.com>
2024-10-29 16:29:49 -0600

    changes installed fonts, stops installing brave gpg

--- my-scripts/install-base-pkgs.sh
+++ my-scripts/install-base-pkgs.sh
@@ -6,23 +6,13 @@
 \
 \
 && sudo apt update \
-&& sudo apt install curl gammastep gh sway swaybg swayidle swaylock xdg-desktop-portal-wlr xwayland waybar brightnessctl feh rofi mako-notifier grimshot neovim network-manager-gnome ntpdate alacritty playerctl fonts-font-awesome neofetch btop copyq python3 pipx wget wlsunset -y \
+&& sudo apt install curl gammastep gh sway swaybg swayidle swaylock xdg-desktop-portal-wlr xwayland waybar brightnessctl feh rofi mako-notifier grimshot neovim network-manager-gnome ntpdate alacritty playerctl fonts-font-awesome fonts-noto-color-emoji fonts-clear-sans neofetch btop copyq python3 pipx wget wlsunset -y \
 && pipx install autotiling \
 && eval "$(register-python-argcomplete pipx)" \
 \
 \
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg \
-\
-\
 && sudo ntpdate pool.ntp.org \
 && sudo hwclock --systohc --utc \
-\
-\
-&& wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Iosevka.zip \
-&& cd ~/.local/share/fonts \
-&& unzip -n Iosevka.zip \
-&& rm Iosevka.zip \
-&& sudo fc-cache -fv \
 \
 \
 && curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim \
