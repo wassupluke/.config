d5f887eff861cba1cb1d9b1554fe97bbb9a62fe9
Luke <wassupluke@gmail.com>
2025-01-09 17:16:48 -0700

    add AutoComplPop, restructure layout

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,58 +1,65 @@
 vim.cmd([[
+"
+" -----------------------------------------------------------------------
 " Install Plug
+
 if empty(glob('~/.vim/autoload/plug.vim'))
   silent !curl -fLo ~/.vim/autoload/plug.vim --create-dirs
     \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
 endif
 
+"
 " -----------------------------------------------------------------------
 " Plugins
-" Vim-Plug Plugin Manager
+
 call plug#begin('~/.vim/plugged')
 
-" File Tree
 Plug 'preservim/nerdtree'
-" FZF File Finder
 Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
 Plug 'junegunn/fzf.vim'
 Plug 'wakatime/vim-wakatime'
-" Visuals
-" ---
-" Improved status line
 Plug 'itchyny/lightline.vim'
-" Color theme
 Plug 'morhetz/gruvbox'
-" Git hunk status in the gutter
 Plug 'airblade/vim-gitgutter'
-" Allow viewing of Hex colors in the buffer [ #fff ]
 Plug 'norcalli/nvim-colorizer.lua'
 Plug 'dense-analysis/ale' " async linting
 Plug 'gabrielelana/vim-markdown'
 Plug 'williamboman/mason.nvim'
 Plug 'williamboman/mason-lspconfig.nvim'
 Plug 'neovim/nvim-lspconfig'
+Plug 'vim-scripts/AutoComplPop'
 
 call plug#end()
-" -----------------------------------------------------------------------
+
 "
+" -----------------------------------------------------------------------
+" Mason
+
 lua require("mason").setup()
 lua require("mason-lspconfig").setup()
 
+"
 " -----------------------------------------------------------------------
-" Ale
+" ALE
 
 let g:ale_linters = {'css': ['csslint', 'vscodecss'], 'html': ['angular', 'htmlhint', 'tidy', 'vscodehtml'], 'python': ['flake8', 'pydocstyle', 'bandit', 'mypy'], 'markdown': ['markdownlint']}
+
 let g:ale_fixers = {'*': ['remove_trailing_lines', 'trim_whitespace'], 'css': ['css-beautify', 'prettier'], 'html': ['html-beautify', 'prettier', 'tidy'], 'python': ['black', 'isort'], 'markdown': ['prettier', 'remark-lint']}
+
 let g:ale_fix_on_save = 1
 
-" -----------------------------------------------------------------------
 "
-" Native Config
-" -------------
+" -----------------------------------------------------------------------
+" Settings
 
 syntax on
 filetype plugin indent on
+
+set spelllang=en
+set complete+=kspell
+set completeopt=menuone,longest,preview
+set shortmess+=c
 
 set mouse=a
 
@@ -75,21 +82,24 @@ set autoindent
 " (╯°□°）╯︵ ┻━┻
 set termguicolors
 
-" Enable copying
 set clipboard=unnamedplus
 
+"
+" -----------------------------------------------------------------------
 " Spell Checking
-" --------------
 
-" F11 Toggle Spell Check
+" F11 Toggles Spell Check
 nnoremap <silent> <F11> :set spell!<cr>
 inoremap <silent> <F11> <C-O>:set spell!<cr>
-set spelllang=en
 
+"
+" -----------------------------------------------------------------------
 " Plugin Variables
-" ----------------
 
+"
+" ---------
 " Git Gutter
+
 set signcolumn=yes
 
 nmap ]h <Plug>(GitGutterNextHunk) "same as default
@@ -101,7 +111,10 @@ let g:gitgutter_sign_removed = '-'
 let g:gitgutter_sign_removed_first_line = '-'
 let g:gitgutter_sign_modified_removed = '-'
 
+"
+" ------------
 " Gruvbox
+
 let g:gruvbox_contrast_dark = 'hard'
 let g:gruvbox_invert_tabline = '1'
 
@@ -110,24 +123,32 @@ let g:lightline = {
       \ 'colorscheme': 'gruvbox',
       \ }
 
-" Enable Svelete plugin
+colorscheme gruvbox
+
+"
+" ---------
+" Svelete
+
 let g:vim_svelte_plugin_load_full_syntax = 1
 let g:svelte_preprocessors = ['typescript']
 
+"
+" ---------
 " Browser for Markdown
+
 let g:mkdp_browser = 'firefox'
 
+"
+" -----------------------------------------------------------------------
 " Bindings
-" --------
 
-
-" [ ; ] - Open FZF Finder
+" [ ; ] opens FZFinder
 map ; :Files<CR>
 
-" [ Control + n ] - Toggle NERDTree
+" [ Control + n ] toggles NERDTree
 map <C-n> :NERDTreeToggle<CR>
 
-" [ Control + c ] - Copy Visual Selection to wl-copy
+" [ Control + c ] copies Visual Selection to wl-copy
 vmap <C-c> y:call system("wl-copy", getreg("\""))<CR>
 
 " Switch between windows with [ Control + h|j|k|l ]
@@ -139,7 +160,4 @@ nnoremap <C-h> <C-W>h
 " Centered half page up / down
 nnoremap <silent> <C-u> <C-u>zz
 nnoremap <silent> <C-d> <C-d>zz
-
-" Colorscheme
-colorscheme gruvbox
 ]])
